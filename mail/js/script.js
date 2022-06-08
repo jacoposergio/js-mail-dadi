8ce7ee421812005716e43528f683ce2abd157834
jacoposergio <djago32@gmail.com>
2022-06-08 15:45:27 +0200

    inizio mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,13 @@
-alert("ciao")
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// scrivo la lista di email registrate
+const emailList = ['red@gmail.com','pink@gmail.com','white@gmail.com','blue@gmail.com','cyan@gmail.com','purple@gmail.com','orange@gmail.com','gray@gmail.com'] 
+
+// chiedo all'utente la sua email 
+const userEmail = prompt('Dimmi la tua email');
+console.log(userEmail);
+
+/*sccorro nell'array in cerca della mail dell'utente
+
