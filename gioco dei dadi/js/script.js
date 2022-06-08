1cde495894e3aac00dbd167cd700318a6d962d17
jacoposergio <djago32@gmail.com>
2022-06-08 17:36:26 +0200

    gioco dadi finito

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -17,10 +17,34 @@ submitButton.addEventListener('click',
         // generare anche il numero del computer
         let computerNumber = parseInt(Math.floor(Math.random() * 6) + 1 );
 
+        
+
         // stampare i numeri
         document.getElementById('your-number').innerHTML = `Il tuo numero è ${userNumber}`;
         document.getElementById('computer-number').innerHTML =`Il numero del computer è ${computerNumber}`;
+        // rendo visibili le classi css 
         document.querySelector('#your-number').classList.add('visible');
         document.querySelector('#computer-number').classList.add('visible');
+
+        // data la variabile userMessagge impostata su pareggio
+        // se il numero utente è maggiore del numero del computer avrà vinto
+        // se no avrà perso
+        // e stampo il messaggio nel DOM
+
+
+        let userMessage = 'HAI PAREGGIATO';
+
+        if (userNumber > computerNumber) {
+            userMessage = 'HAI VINTO';
+        } else if (userNumber < computerNumber) {
+            userMessage = 'HAI PERSO';
+        }
+        console.log(userMessage);
+
+        // stampo
+
+        document.getElementById('user-message').innerHTML = userMessage;
     }
 );
+
+
